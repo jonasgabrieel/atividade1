e25c20c977d91c4236eab5dc51a049d3ace9cf75
Jonas Gabrieel <86636633+jonasgabrieel@users.noreply.github.com>
2024-09-08 22:13:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -50,4 +50,4 @@ def excluir_usuario(id):
    return jsonify(usuarios)
 
 if __name__ == '__main__':
-    app.run(port=5000, host='localhost', debug=True)
+    app.run(port=5000, host='0.0.0.0', debug=True)
